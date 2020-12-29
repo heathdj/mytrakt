acf119d6983e712120ee506c73040d32157dc8a3
Dave Heath <heathdj@gmail.com>
2020-12-28 21:45:30 -0700

    Parking for the night

--- api/deps.ts
+++ api/deps.ts
@@ -1,2 +1,2 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
-export { MongoClient, Bson } from "https://deno.land/x/mongo@v0.20.1/mod.ts";
+export { MongoClient, Bson } from "https://deno.land/x/mongo/mod.ts";
